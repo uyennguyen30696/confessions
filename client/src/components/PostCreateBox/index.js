1ef3f2f6b2d43bbcf7bc55c68532f52d47a28bdc
Uyen Nguyen <udnguyen@ucsd.edu>
2021-03-27 22:49:39 -0700

    change import API from profileAPI to newsfeedAPI

--- client/src/components/PostCreateBox/index.js
+++ client/src/components/PostCreateBox/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./style.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import API from "../../utils/profileAPI";
+import API from "../../utils/newsfeedAPI";
 import APIuser from "../../utils/userAPI";
 
 function PostCreateBox() {
